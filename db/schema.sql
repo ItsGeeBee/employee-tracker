3b3292b8f2a96c608d2f645386921062c32c9484
ItsGeeBee <gracelbrown@hotmail.com>
2022-08-02 17:33:51 +1000

    Continued working on switch statment. Created table role in Schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,20 @@
-DROP DATABASE IF EXISTS employee_tracker_db;
-CREATE DATABASE employee_tracker_db;
+DROP DATABASE IF EXISTS employee_tracker_db; --Prevent an error from occurring if the database does not exist
+CREATE DATABASE employee_tracker_db; -- Create database under this name 
 
-USE employee_tracker_db;
+USE employee_tracker_db; -- use this data base 
 
+-- Create table for each department
 CREATE TABLE department (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id) -- Set primary key 
+);
+-- Create table for each role
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) -- used to link 2 tables together 
+    REFERENCES department(id) -- Referencing 'department' table 
 );
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+
+// Require needed packages 
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 
+// Create connection to database
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3001,
@@ -9,6 +12,7 @@ const connection = mysql.createConnection({
     database: "employee_tracker_db"
 });
 
+// propmts using inquirer 
 function menu() {
     inquirer.prompt({
     name: "menu",
@@ -17,18 +21,22 @@ function menu() {
     choices: [ "View all departments", "View all roles", "View all employees", "Add a department", 
     "Add a role","Add an employee", "Update employee role","Exit"]
 })
+
+// switch statement used to cycle through reponses based on user selection 
 .then((res) => {
     switch (res.Menu) {
         case 'View All Employees':
             viewEmployees();
             break;   
+        case "View all departments":
+            viewDepts();
+            break;
 
 
 
 
 
-
-
+    };
 
 
 
@@ -39,6 +47,15 @@ function viewEmployees() {
     connection.query("SELECT * FROM employee", (err, data) => {
         if (err) throw err;
         console.log("Displaying all employees:");
+        console.table(data);
+        start();
+    });
+}
+
+function viewDepts() {
+    connection.query("SELECT * FROM department", (err, data) => {
+        if (err) throw err;
+        console.log("Displaying all departments:");
         console.table(data);
         start();
     });
